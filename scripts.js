e610f5fce59685b819a860618e3b5381d34494d3
franksal82 <franksalazar82@gamil.com>
2016-01-20 16:05:59 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// var num1 = prompt("give me a number");
+// var num2 = prompt("how about a second number");
+//
+// if(!isNaN(num1) && !isNaN(num2)) {
+//   num1 = parseInt(num1);
+//   num2 = parseInt(num2);
+//
+//   var answer = num1 + num2;
+//   alert(answer);
+//
+// } else {
+//     alert("Invalid input")
+// }
+var whatDo = prompt("What would you like to do?");
+if(whatDo != "") {
+
+  alert(whatDo);
+} else {
+  alert("Next time enter a valid input.")
+}
